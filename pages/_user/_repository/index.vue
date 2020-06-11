60ef0f3269282936eb3c49234324aab82c4e4f73
Nina Nazarova <ninanazarova0496@yandex.ru>
2020-06-11 15:28:54 +0300

    initial commit

---
+++ pages/_user/_repository/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>какойто юзер/его репозиторий</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ pages/_user/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 class="title">Этой страницы пока нет))0)0</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+}
+</style>
